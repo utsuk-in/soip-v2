f1c08a94bbf34fcf833e0e1ea811a368335d1113
agalya <agalyadevi.a@utsuk.in>
2026-03-09 18:06:09 +0530

    updated vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 export default defineConfig({
   root: "src",
   publicDir: "../public",
+  preview: {
+    allowedHosts: ["soip-v2-ui-new.onrender.com"],
+  },
   build: {
     outDir: "../dist",
     emptyOutDir: true,
